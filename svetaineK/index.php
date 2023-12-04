d6260d542f16b0b38912bbde4f0935b7ed11ba7a
Tomas <kizelis.tomas@gmail.com>
2023-12-05 01:38:47 +0200

    first commit

---
+++ svetaineK/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="Sukūrimo data" content="11/16/2023">
+    <meta name="Galiojimo data" content="iki 09/16/2025">
+    <meta name="Autorius" content="Milanas Orševskis">
+    <meta name="Rights" content="All Rights Reserved">
+    <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Montserrat:400,700&amp;subset=cyrillic-ext" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Combo&display=swap" rel="stylesheet">
+   
+    <title>Kirpykla</title>
+</head>
+
+<body>
+   <header class="header">
+       <div class="container">
+           <div class="header__inner">
+               <a class="logo__link" href="index.php"></a>
+
+               <nav class="nav">
+                   <a class="nav__link" href="index.php">home</a>
+                   <a class="nav__link" href="Contact.html">contact</a>
+               </nav>
+               
+           </div>
+       </div>
+   </header>
+    
+    <div class="intro">
+    
+        <div class="container">
+           <div class="intro__inner">
+                
+              <h1 class="intro__title">Kirpykla</h1>
+           </div>
+        </div>
+    
+    </div>
+    
+</body>
+</html>
+
+
+
+
+
+
+
